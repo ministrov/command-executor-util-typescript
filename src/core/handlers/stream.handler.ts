cf43bcab601949273df8ca5d125e42b4a3bf7cef
Zhila <ministrov2018@gmail.com>
2024-11-26 17:27:15 +0300

    feature/finish the project

--- src/core/handlers/stream.handler.ts
+++ src/core/handlers/stream.handler.ts
@@ -2,15 +2,15 @@ import { ChildProcessWithoutNullStreams } from "child_process";
 import { IStreamLogger } from "./stream-logger.interface";
 
 export class StreamHandler {
-  constructor(private logger: IStreamLogger) {}
+  constructor(private logger: IStreamLogger) { }
 
   processOutput(stream: ChildProcessWithoutNullStreams) {
     stream.stdout.on('data', (data: any) => {
-      this.logger.log(data);
+      this.logger.log(data.toString());
     })
 
     stream.stderr.on('data', (data: any) => {
-      this.logger.error(data);
+      this.logger.error(data.toString());
     })
 
     stream.on('close', () => {
